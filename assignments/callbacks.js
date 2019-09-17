33c55220bc6dea4b48d806e23e95d4f596e0695a
paulmjensen <40579292+paulmjensen@users.noreply.github.com>
2019-09-17 19:02:18 -0400

    Revert "Initial Commit"

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,14 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
-  return cb(arr[0]);
-}
-  firstItem(items, (whatever)=>{
-    console.log(whatever);
-  })
-
-
+/* 
 
   // GIVEN THIS PROBLEM:
 
@@ -43,51 +36,30 @@ function firstItem(arr, cb) {
 
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
-
+*/
 
 
 function getLength(arr, cb) {
-  cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
-getLength(items, (bananaphone) => {
-  console.log(bananaphone);
-});
 
 function last(arr, cb) {
-  cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 }
-last(items, (hamburgerphone) =>{
-  console.log(hamburgerphone);
-})
 
 function sumNums(x, y, cb) {
-  cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
-sumNums(4, 5, (TheSum) =>{
-  console.log(TheSum);
-})
 
 function multiplyNums(x, y, cb) {
-  cb(x * y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
-multiplyNums(9, 10, (Multiply) =>{
-  console.log(Multiply);
-})
 
-// function contains(item, list, cb) {
-//   cb(list.ForEach((item, list)=>{
-//     alert(item, list);
-//   }))
-//   // contains checks if an item is present inside of the given array/list.
-//   // Pass true to the callback if it is, otherwise pass false.
-// }
-// contains(anything, items, (UMMMM) => {
-//   console.log(UMMMM);
-// }
+function contains(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+}
+
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
